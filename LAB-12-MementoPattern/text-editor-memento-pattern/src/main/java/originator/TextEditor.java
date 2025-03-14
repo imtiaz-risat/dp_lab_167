c7ef3f78123632ddfff680140271b5080a8fd588
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-14 14:18:40 +0600

    LAB 12: Basic TextEditor functionality

--- LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/originator/TextEditor.java
+++ LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/originator/TextEditor.java
@@ -1,5 +1,34 @@
 package originator;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class TextEditor {
-    
+    private String content;
+    private int cursorPosition;
+    private List<String> selections;
+
+    public TextEditor() {
+        this.content = "";
+        this.cursorPosition = 0;
+        this.selections = new ArrayList<>();
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public void setCursorPosition(int cursorPosition) {
+        this.cursorPosition = cursorPosition;
+    }
+
+    public void addSelection(String selection) {
+        this.selections.add(selection);
+    }
+
+    public void displayState() {
+        System.out.println("Content: " + content);
+        System.out.println("Cursor Position: " + cursorPosition);
+        System.out.println("Selections: " + selections);
+    }
 }
