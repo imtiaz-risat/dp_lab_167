5075046fc161640c8f8a6bda16220e933d902a58
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-14 15:11:16 +0600

    LAB 12: Added saveToFile and loadFromFile in History

--- LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/caretaker/History.java
+++ LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/caretaker/History.java
@@ -2,6 +2,12 @@ package caretaker;
 
 import originator.TextEditor;
 import memento.EditorMemento;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,5 +30,22 @@ public class History {
             EditorMemento memento = mementos.remove(mementos.size() - 1);
             textEditor.restoreFromMemento(memento);
         }
+    }
+
+    public void saveToFile(String filePath) throws IOException {
+        // TODO: Implement save to file logic
+        FileOutputStream fileOutputStream = new FileOutputStream(filePath);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+        objectOutputStream.writeObject(mementos);
+        objectOutputStream.close();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void loadFromFile(String filePath) throws IOException, ClassNotFoundException {
+        // TODO: Implement load from file logic
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+        mementos = (List<EditorMemento>) objectInputStream.readObject();
+        objectInputStream.close();
     }
 }
