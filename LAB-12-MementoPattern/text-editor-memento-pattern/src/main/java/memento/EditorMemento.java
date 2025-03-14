2b313ad8aed930cdc6fbe70a7969d923a7fa7751
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-14 14:25:03 +0600

    LAB 12: Added Properties and Constructor in EditorMemento

--- LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/memento/EditorMemento.java
+++ LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/memento/EditorMemento.java
@@ -1,7 +1,16 @@
 package memento;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class EditorMemento implements Serializable {
-    
+    private String content;
+    private int cursorPosition;
+    private List<String> selections;
+
+    public EditorMemento(String content, int cursorPosition, List<String> selections) {
+        this.content = content;
+        this.cursorPosition = cursorPosition;
+        this.selections = selections;
+    }
 }
