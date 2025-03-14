5f722b186b4a4e2cb075ae60366eef9222b06a4d
imtiaz-risat <ahababimtiaz@iut-dhaka.edu>
2025-03-14 14:47:13 +0600

    LAB 12: Added backup() and undo() in Hisory

--- LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/caretaker/History.java
+++ LAB-12-MementoPattern/text-editor-memento-pattern/src/main/java/caretaker/History.java
@@ -1,5 +1,28 @@
 package caretaker;
 
+import originator.TextEditor;
+import memento.EditorMemento;
+import java.util.ArrayList;
+import java.util.List;
+
 public class History {
-    
+    private TextEditor textEditor;
+    private List<EditorMemento> mementos;
+
+    public History(TextEditor textEditor) {
+        this.textEditor = textEditor;
+        this.mementos = new ArrayList<>();
+    }
+
+    public void backup() {
+        EditorMemento memento = textEditor.createMemento();
+        mementos.add(memento);
+    }
+
+    public void undo() {
+        if (!mementos.isEmpty()) {
+            EditorMemento memento = mementos.remove(mementos.size() - 1);
+            textEditor.restoreFromMemento(memento);
+        }
+    }
 }
